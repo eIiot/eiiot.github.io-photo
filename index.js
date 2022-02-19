3794ea74c6605e0deaf0a680de1cf15aa415efeb
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-02-18 18:20:33 -0800

    make more minimalist, delete bad photos

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const left = document.getElementById('left');
 const right = document.getElementById('right');
 const img = document.querySelector('img');
 
-// filters
-const urbanFilter = document.getElementById('urban-filter');
-const landscapeFilter = document.getElementById('landscape-filter');
-const allFilter = document.getElementById('all-filter');
-
 const allImages = {
-  '1.jpg': {description: "Man on Beach", location: "Long Beach, CA", tags: ["landscape"]},
+  '1.jpg': {description: "Beach", location: "Long Beach, CA", tags: ["landscape"]},
   '0.jpg': {description: "Sunset", location: "San Francisco, CA", tags: ["landscape"]},
   '2.jpg': {description: "Field of Lights", location: "Paso Robles, CA", tags: ["landscape"]},
-  '3.jpg': {description: "Chicago", location: "Chicago, IL", tags: ["urban"]},
   '4.jpg': {description: "Rooftop", location: "Chicago, CA", tags: ["urban"]},
   '5.jpg': {description: "Sunset", location: "Berkeley, CA", tags: ["landscape"]},
   '6.jpg': {description: "Smoke", location: "Berkeley, CA", tags: ["urban"]},
@@ -60,31 +54,3 @@ right.onclick = function() {
   img.src = './img/' + nextImage;
   img.title = images[nextImage].description + '\n' + images[nextImage].location;
 };
-
-// FILTERS
-urbanFilter.onclick = function() {
-  urbanFilter.classList.add('active-btn');
-  landscapeFilter.classList.remove('active-btn');
-  allFilter.classList.remove('active-btn');
-  images = urbanImages;
-  img.src = './img/' + Object.keys(urbanImages)[0];
-  img.title = urbanImages[Object.keys(urbanImages)[0]].description + '\n' + urbanImages[Object.keys(urbanImages)[0]].location;
-};
-
-landscapeFilter.onclick = function() {
-  landscapeFilter.classList.add('active-btn');
-  urbanFilter.classList.remove('active-btn');
-  allFilter.classList.remove('active-btn');
-  images = landscapeImages;
-  img.src = './img/' + Object.keys(landscapeImages)[0];
-  img.title = landscapeImages[Object.keys(landscapeImages)[0]].description + '\n' + landscapeImages[Object.keys(landscapeImages)[0]].location;
-};
-
-allFilter.onclick = function() {
-  allFilter.classList.add('active-btn');
-  urbanFilter.classList.remove('active-btn');
-  landscapeFilter.classList.remove('active-btn');
-  images = allImages;
-  img.src = './img/' + Object.keys(allImages)[0];
-  img.title = allImages[Object.keys(allImages)[0]].description + '\n' + allImages[Object.keys(allImages)[0]].location;
-}
